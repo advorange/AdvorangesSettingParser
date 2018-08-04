d14481213724eafc92eb65ed563ef4f2c017fb79
Advorange <advorange@live.com>
2018-08-04 01:00:47 -0700

    Forgot to upload the code, also updated the properties so it has the correct links and author, etc.

---
+++ src/AdvorangesSettingParser/TryParseDelegate.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvorangesSettingParser
+{
+	/// <summary>
+	/// Attempts to convert the string to the supplied type.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="s"></param>
+	/// <returns></returns>
+	public delegate (bool Success, T Value) TryParseDelegate<T>(string s);
+}
